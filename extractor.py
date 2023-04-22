93d835c8a1fd69f2554b3706fee6a780eb4abe4d
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 07:02:39 +0800

    Look for numbers.txt file

--- extractor.py
+++ extractor.py
@@ -11,3 +11,15 @@ This program separates the odd and even numbers into categorized text files.
 3. Remember to close the txt file! (Use with operator for a foolproof execution!)
 
 '''
+
+def open_file_and_extract():
+
+    # Look for numbers.txt file
+    try:
+        with open('numbers.txt') as numbers:
+            for line in numbers:
+                print(line.strip())
+    except FileNotFoundError:
+        return 'File numbers.txt is not found'
+
+print(open_file_and_extract())
