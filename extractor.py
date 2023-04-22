0117bd1e3af38e337f4d0ffccfbd4239b47e0531
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 07:24:05 +0800

    Segregate evens and odds

--- extractor.py
+++ extractor.py
@@ -16,12 +16,32 @@ def open_file_and_extract():
 
     # Look for numbers.txt file
     try:
-        with open('numbers.txt') as numbers:
+        with open('numbers.txt', 'r') as numbers:
             # Iterate through every number from numbers.txt
-            for line in numbers:
-                print(line.strip())
+            numbers = numbers.readlines()
 
+            for index, number in enumerate(numbers):
+                # If number is even
+                if(int(number.strip()) % 2 == 0):
+                    store_to_file(str(number), index, 'even')
+                # If number is odd
+                else:
+                    store_to_file(str(number), index, 'odd')
     except FileNotFoundError:
         return 'File numbers.txt is not found'
+
+def store_to_file(number, index, mode):
+
+    # Determine divisor according to mode
+    divisor = 1 if mode == 'odd' else 0
+    
+    # If number is first on list, create a new file or overwrite existing
+    if index == 0:
+        with open(f'{mode}.txt', 'w') as even:
+            even.write(str(number))
+    else:
+        with open(f'{mode}.txt', 'a') as even:
+            even.write(str(number))
+
 
 print(open_file_and_extract())
