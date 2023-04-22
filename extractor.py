b91286d6d89e560354079dd49db317c51c15ae2a
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 06:53:24 +0800

    Initial commit

---
+++ extractor.py
@@ -0,0 +1,13 @@
+'''
+
+file: extractor.py
+
+This program separates the odd and even numbers into categorized text files.
+
+1. Look for numbers.txt file
+2. Iterate through every number from numbers.txt
+    - If even, store it on even.txt
+    - If odd, store it on odd.txt
+3. Remember to close the txt file! (Use with operator for a foolproof execution!)
+
+'''
