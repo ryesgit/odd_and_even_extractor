330f33864a5462a44fd28a0b0daec3a1ece6dbcc
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 07:47:09 +0800

    Create or overwrite files for every execution
    Before, new numbers are appended to already created files.
    
    This new feature gives the ability to overwrite  to avoid pollution

--- extractor.py
+++ extractor.py
@@ -12,6 +12,7 @@ This program separates the odd and even numbers into categorized text files.
 
 '''
 
+
 def open_file_and_extract():
 
     # Look for numbers.txt file
@@ -23,25 +24,36 @@ def open_file_and_extract():
             for index, number in enumerate(numbers):
                 # If number is even
                 if(int(number.strip()) % 2 == 0):
-                    store_to_file(str(number), index, 'even')
+                    store_to_file(str(number), 'even')
                 # If number is odd
                 else:
-                    store_to_file(str(number), index, 'odd')
+                    store_to_file(str(number), 'odd')
     except FileNotFoundError:
         return 'File numbers.txt is not found'
 
-def store_to_file(number, index, mode):
+def store_to_file(number, mode):
+
+    global first_odd, first_even
 
     # Determine divisor according to mode
     divisor = 1 if mode == 'odd' else 0
-    
-    # If number is first on list, create a new file or overwrite existing
-    if index == 0:
-        with open(f'{mode}.txt', 'w') as even:
-            even.write(str(number))
-    else:
-        with open(f'{mode}.txt', 'a') as even:
-            even.write(str(number))
 
+    # If number is first on list, create a new file or overwrite existing
+    if not (first_odd and first_even):
+        # These will determine whether there the first odd or even have already been identified
+        if not first_odd and mode == 'odd':
+            first_odd = True
+        
+        if not first_even and mode == 'even':
+            first_even = True
+
+        with open(f'{mode}.txt', 'w') as odd_or_even_file:
+            odd_or_even_file.write(str(number))
+    else:
+        with open(f'{mode}.txt', 'a') as odd_or_even_file:
+            odd_or_even_file.write(str(number))
+
+first_odd = False
+first_even = False
 
 print(open_file_and_extract())
