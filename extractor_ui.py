645fd6fbed8c6c3e98c4429b61ac6528127795b0
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 08:05:42 +0800

    Establish Extractor UI

---
+++ extractor_ui.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+
+TITLE = "Even And Odd Extractor"
+
+sg.theme('GrayGrayGray')
+
+window = sg.Window(TITLE)
+
+layout = [
+    [sg.Text('', size=(20,1)), sg.Text(TITLE, font=('Any 20'), auto_size_text=True, justification='center'), sg.Text('', size=(20,1))],
+    [sg.Text('File name to open: ')],
+    [sg.Input(), sg.FileBrowse()],
+    [sg.Button('Upload'), sg.Button('Quit')]
+]
+
+window.layout(layout)
+
+while True:
+    event, values = window.read()
+
+    if(event == sg.WIN_CLOSED or event == 'Quit'):
+        break
+
+    if(event == 'Upload'):
+        print(values[0])
+
+window.close()
